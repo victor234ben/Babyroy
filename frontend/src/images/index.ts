96ea0b14c476bf39c80ffb1f7eb664f784243901
Benjamin <benferanmiopafunso@gmail.com>
2025-05-15 20:59:31 +0100

    babyroy imgae agjustment

--- frontend/src/images/index.ts
+++ frontend/src/images/index.ts
@@ -13,6 +13,6 @@ import taskWhitePaws from "./task-white-paws.webp";
 import taskBoost from "./task-boost.webp";
 import taskBlum from "./task-blum.webp";
 import tokenIcon from "./tokenIcon.webp";
-import babyRoyBg  from "./BabyRoyBg.jpg";
+import babyRoyBg  from "./babyRoyBg.jpg";
 
 export { sparkles, trophy, tokenIcon, taskWhitePaws, taskBoost, taskBlum, babyRoyBg };
