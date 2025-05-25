55e07b140a3352e0d92a429e894a60aa0d4f7618
Benjamin <benferanmiopafunso@gmail.com>
2025-05-25 12:19:13 +0100

    sunday one

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -39,6 +39,13 @@ const App = () => {
     }
   }, []);
 
+   useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://telegram.org/js/telegram-web-app.js';
+    script.async = true;
+    document.head.appendChild(script);
+  }, []);
+
   return (
     <TonConnectUIProvider manifestUrl={tonConnectConfig.manifestUrl}>
       <QueryClientProvider client={queryClient}>
