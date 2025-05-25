4e8853bf1004ce2f2fa0058190dfc0b913859ce0
Benjamin <benferanmiopafunso@gmail.com>
2025-05-25 20:52:38 +0100

    fixting sunday 2

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,14 +4,15 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: './', // 👈 ADD THIS LINE
+  base: "./", // 👈 ADD THIS LINE
+  build: {
+    outDir: "dist",
+  },
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-  ].filter(Boolean),
+  plugins: [react()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
